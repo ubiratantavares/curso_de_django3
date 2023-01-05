442b348abccaeca9245daa50e0f4bd93072f538b
= <=>
2023-01-05 09:46:37 -0300

    alteracoes nas classes Categoria e Produto

--- django/categoria/admin.py
+++ django/categoria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Categoria
 
-# Register your models here.
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'slug']
+    search_fields = ['nome', 'slug']
+    prepopulated_fields = {'slug': ('nome',)}
+
+
+admin.site.register(Categoria, CategoriaAdmin)
--- django/produto/admin.py
+++ django/produto/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Produto
 
-# Register your models here.
+
+class ProdutoAdmin(admin.ModelAdmin):
+    fields = ('categoria', 'nome', 'slug', 'imagem', 'qtd_estoque', 'preco', 'disponivel')
+    list_display = ['nome', 'slug', 'categoria', 'imagem', 'qtd_estoque', 'preco', 'disponivel']
+    search_fields = ['nome', 'imagem']
+    list_filter = ['categoria']
+    list_editable = ['categoria', 'imagem', 'qtd_estoque', 'preco', 'disponivel']
+    prepopulated_fields = {'slug': ('nome',)}
+
+
+admin.site.register(Produto, ProdutoAdmin)
